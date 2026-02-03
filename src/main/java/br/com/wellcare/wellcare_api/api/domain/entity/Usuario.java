87b34170aa2624c14f07d1d633ad1456b3015b41
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-02-03 20:25:28 -0300

    Start Project wellcare-api

---
+++ src/main/java/br/com/wellcare/wellcare_api/api/domain/entity/Usuario.java
@@ -0,0 +1,8 @@
+package br.com.wellcare.wellcare_api.api.domain.entity;
+
+/**
+ *
+ */
+public class Usuario {
+
+}
---
+++ src/test/java/br/com/wellcare/wellcare_api/WellcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellcare.wellcare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
