e624981a28674ad8ca3b706551788ecdf9bd77e7
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-02-03 21:07:11 -0300

    feat: Implementado classe representativa de Usuário

--- src/main/java/br/com/wellcare/wellcare_api/api/domain/entity/Usuario.java
+++ src/main/java/br/com/wellcare/wellcare_api/api/domain/entity/Usuario.java
@@ -1,8 +1,39 @@
 package br.com.wellcare.wellcare_api.api.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 /**
- *
+ * Entidade que representa um usuário do sistema wellcare.
+ * Pode ser um paciente ou aluno acompanhado por profissionais.
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
 
 }
